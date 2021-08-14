3d561e52112e6cec08ce6b815d0db291cf59581a
pattana <pattana.ps@gmail.com>
2021-08-14 15:57:50 +0700

    Add Project Flask

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello Flask!</p>"
+
+@app.route("/gallery")
+def gallery():
+    return render_template("gallery.html")
+
+
+
+
